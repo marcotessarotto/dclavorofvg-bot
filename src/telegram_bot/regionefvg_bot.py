aa2cc7008b977f6cc98ef158b9275506b8c1691e
Marco T <marcotts@gmail.com>
2019-10-24 10:31:52 +0200

    send_news update

--- src/telegram_bot/regionefvg_bot.py
+++ src/telegram_bot/regionefvg_bot.py
@@ -1,8 +1,3 @@
-
-# cd Scrivania/regionefvg_bot
-# export PYTHONPATH=.:src/telegram_bot
-# python src/telegram_bot/regionefvg_bot.py
-
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, KeyboardButton, ReplyKeyboardMarkup, Bot, \
     ChatAction
 
@@ -27,44 +22,116 @@ from telegram.error import (TelegramError, Unauthorized, BadRequest,
 
 from src.gfvgbo.settings import MEDIA_ROOT
 
+import mimetypes
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+#
 global_bot_instance = None
 
-file_id_dict = {}
+# dictionary of file_id cache, used when uploading files to telegram server
+file_id_cache_dict = {}
 
 
-def _process_message_lookup_file_id(message, _file_path):
+def _lookup_file_id_in_message(message, _file_path: str, file_id):
+    """search for file_id in message, if parameter file_id is not None;
+    if found, store it in file_id cache (associate it to file_path)"""
+    if file_id is not None:
+        return
+
     _file_id = None
+    # Python community recommend a strategy of "easier to ask for forgiveness than permission" (EAFP) rather than "look before you leap" (LBYL)
+    # https://stackoverflow.com/a/610923/974287
     try:
         _file_id = message.document.file_id
     except AttributeError:
         try:
-            if message.photo is not None:
-                _file_id = message.photo[0].file_id
-                # print(type(_file_id))
-                # print(_file_id.file_id)
+            # if message.photo is not None:
+            _file_id = message.photo[0].file_id
         except AttributeError:
             print("_process_message_lookup_file_id: cannot find file_id")
             my_print(message, 4)
-            return None
+            return
 
     if _file_id is not None:
-        file_id_dict[_file_path] = _file_id
+        file_id_cache_dict[_file_path] = _file_id
 
     print("process_message_for_file_id: " + str(_file_id) + " for " + _file_path)
 
-    pass
+    return
 
 
 def _get_file_id_for_file_path(_file_path):
-    if _file_path in file_id_dict:
-        return file_id_dict[_file_path]
+    return file_id_cache_dict.get(_file_path)
+
+
+def _send_file_using_mime_type(context, _telegram_user_id: int, _file_path, _file_mime_type: str, caption=None):
+    if caption is None:
+        caption = ''
+
+    if len(caption) > 1024:
+        print("WARNING! len(caption) cannot be > 1024, truncating...")
+        caption = caption[:1024]
+
+    if _file_mime_type is None:
+        _file_mime_type = mimetypes.guess_type(_file_path)[0]
+
+    _file_id = _get_file_id_for_file_path(_file_path)  # is file already present on Telegram servers?
+
+    if _file_mime_type == 'image/gif':
+        _message = context.bot.send_animation(
+            chat_id=_telegram_user_id,
+            animation=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+    elif _file_mime_type.startswith('image'):
+        _message = context.bot.send_photo(
+            chat_id=_telegram_user_id,
+            photo=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+    elif _file_mime_type.startswith('application'):
+        _message = context.bot.send_document(
+            chat_id=_telegram_user_id,
+            document=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+    elif _file_mime_type == 'audio/ogg':
+        _message = context.bot.send_voice(
+            chat_id=_telegram_user_id,
+            voice=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+    elif _file_mime_type == 'video/mp4':
+        _message = context.bot.send_video(
+            chat_id=_telegram_user_id,
+            video=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+    elif _file_mime_type.startswith('audio'):
+        _message = context.bot.send_audio(
+            chat_id=_telegram_user_id,
+            audio=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
     else:
-        return None
+        _message = context.bot.send_document(
+            chat_id=_telegram_user_id,
+            document=_file_id if _file_id is not None else open(_file_path, 'rb'),
+            caption=caption,
+            parse_mode='HTML'
+        )
+
+    _lookup_file_id_in_message(_message, _file_path, _file_id)
 
 
 def check_user_privacy_approval(telegram_user: TelegramUser, update, context):
@@ -747,7 +814,6 @@ def news_dispatcher(context: CallbackContext):
     print("news_dispatcher - finished processing all news - dt=" + str(c.microseconds) + " microseconds")
 
 
-# SEZIONE INVIO NEWS
 # ****************************************************************************************
 
 def send_news_to_telegram_user(context, news_item : NewsItem, telegram_user: TelegramUser, intersection_result, request_feedback=True,
@@ -755,9 +821,9 @@ def send_news_to_telegram_user(context, news_item : NewsItem, telegram_user: Tel
     print(
         "send_news_to_telegram_user - news_item=" + str(news_item.id) + ", telegram_user=" + str(telegram_user.user_id))
 
-    a = datetime.datetime.now()
+    telegram_user_id = telegram_user.user_id
 
-    # build html content
+    a = datetime.datetime.now()
 
     # title_html_content = ''
     categories_html_content = ''
@@ -818,9 +884,10 @@ def send_news_to_telegram_user(context, news_item : NewsItem, telegram_user: Tel
     if news_item.link is not None:
         link_html_content = '... <a href=\"' + news_item.link + '\">' + news_item.link_caption + '</a>'
 
-    html_content = title_html_content + categories_html_content + body_html_content + link_html_content
+    # complete html body of news item (separate from title. link...)
+    html_news_content = title_html_content + categories_html_content + body_html_content + link_html_content
 
-    print("send_news_to_telegram_user - len(html_content) = " + str(len(html_content)))
+    print("send_news_to_telegram_user - len(html_content) = " + str(len(html_news_content)))
 
     # print("len(title_html_content) = " + str(len(title_html_content)))
     # print("len(categories_html_content) = " + str(len(categories_html_content)))
@@ -828,105 +895,93 @@ def send_news_to_telegram_user(context, news_item : NewsItem, telegram_user: Tel
     # print("len(link_html_content) = " + str(len(link_html_content)))
 
     file1_is_image = False
-    file_id = None # is file already present on Telegram servers?
+    # file_id = None  # is file already present on Telegram servers?
 
     # file1 is present, check if it is an image
     if news_item.file1 is not None:
         # print(news_item.file1.file_field.name)
         # example: uploads/2019/10/03/500px-Tux_chico.svg_VtRyDrN.png
 
-        image_path = MEDIA_ROOT + news_item.file1.file_field.name
+        file_path = MEDIA_ROOT + news_item.file1.file_field.name
 
-        print("send_news_to_telegram_user - fs path of image to send: " + image_path)
+        print("send_news_to_telegram_user - fs path of file1: " + file_path)
 
-        import mimetypes
-        file_mime_type = mimetypes.guess_type(image_path)[0]
-        # print(file_mime_type)
+        file_mime_type = mimetypes.guess_type(file_path)[0]
+
         file1_is_image = file_mime_type.startswith('image')
-        print("is_image = " + str(file1_is_image))
 
-        file_id = _get_file_id_for_file_path(image_path)
-        print("file_id present on telegram server? " + str(file_id is not None))
+        print("file1 mime_type: " + str(file_mime_type))
+        # print("file1_is_image: " + str(file1_is_image))
 
-    if file1_is_image:
+        # file_id = _get_file_id_for_file_path(file_path)
+        # print("file_id on telegram server: " + str(file_id))
 
-        # LIMIT on caption len: 1024 bytes! or we get MEDIA_CAPTION_TOO_LONG from Telegram
-        # https://core.telegram.org/bots/api#sendphoto
+        if file1_is_image:
 
-        if len(html_content) > 1024:
-            # first, send image associated with news item
-            message = context.bot.send_photo(
-                chat_id=telegram_user.user_id,
-                photo=file_id if file_id is not None else open(image_path, 'rb'),
-                caption='',
-                parse_mode='HTML'
-            )
-            _process_message_lookup_file_id(message, image_path)
+            # LIMIT on caption len: 1024 bytes! or we get MEDIA_CAPTION_TOO_LONG from Telegram
+            # https://core.telegram.org/bots/api#sendphoto
 
-            # second, send content of news item
+            if len(html_news_content) > 1024:
+                # first, send image associated with news item
+                _send_file_using_mime_type(context, telegram_user_id, file_path, file_mime_type)
+
+                # second, send content of news item
+                context.bot.send_message(
+                    chat_id=telegram_user_id,
+                    text=html_news_content,
+                    parse_mode='HTML'
+                )
+            else:
+
+                _send_file_using_mime_type(context, telegram_user_id, file_path, file_mime_type, caption=html_news_content)
+
+        else:  # file1 is present but it is not an image
+            # first, send content of news item
             context.bot.send_message(
-                chat_id=telegram_user.user_id,
-                text=html_content,
+                chat_id=telegram_user_id,
+                text=html_news_content,
                 parse_mode='HTML'
             )
-        else:
-            message = context.bot.send_photo(
-                chat_id=telegram_user.user_id,
-                photo=file_id if file_id is not None else open(image_path, 'rb'),
-                caption=html_content,
-                parse_mode='HTML'
-            )
-            _process_message_lookup_file_id(message, image_path)
 
-    else:
-        # write body of news item
+            # file1 is defined but it is not an image, send it as a document
+            _send_file_using_mime_type(context, telegram_user_id, file_path, file_mime_type)
+
+    else: # file1 is not defined, send only content of news item
         context.bot.send_message(
             chat_id=telegram_user.user_id,
-            text=html_content,
+            text=html_news_content,
             parse_mode='HTML'
         )
-
-    # file1 is defined but it is not an image
-    if news_item.file1 is not None and not file1_is_image:
-        message = context.bot.send_document(
-            chat_id=telegram_user.user_id,
-            document=file_id if file_id is not None else open(image_path, 'rb'),
-            caption='',
-            parse_mode='HTML'
-        )
-        _process_message_lookup_file_id(message, image_path)
-
-        if DEBUG_MSG:
-            print("send_news_to_telegram_user message:")
-            my_print(message, 4)
 
     if news_item.file2 is not None:
         file_path = MEDIA_ROOT + news_item.file2.file_field.name
         print("send_news_to_telegram_user - fs path of file2 to send: " + file_path)
 
-        file_id = _get_file_id_for_file_path(file_path)
+        _send_file_using_mime_type(context, telegram_user_id, file_path, file_mime_type=None)
 
-        message = context.bot.send_document(
-            chat_id=telegram_user.user_id,
-            document=file_id if file_id is not None else open(file_path, 'rb'),
-            caption='',
-            parse_mode='HTML'
-        )
-        _process_message_lookup_file_id(message, file_path)
+        # message = context.bot.send_document(
+        #     chat_id=telegram_user.user_id,
+        #     document=file_id if file_id is not None else open(file_path, 'rb'),
+        #     caption='',
+        #     parse_mode='HTML'
+        # )
+        # _lookup_file_id_in_message(message, file_path, file_id)
 
     if news_item.file3 is not None:
         file_path = MEDIA_ROOT + news_item.file3.file_field.name
         print("send_news_to_telegram_user - fs path of file3 to send: " + file_path)
 
-        file_id = _get_file_id_for_file_path(file_path)
+        _send_file_using_mime_type(context, telegram_user_id, file_path, file_mime_type=None)
 
-        message = context.bot.send_document(
-            chat_id=telegram_user.user_id,
-            document=file_id if file_id is not None else open(file_path, 'rb'),
-            caption='',
-            parse_mode='HTML'
-        )
-        _process_message_lookup_file_id(message, file_path)
+        # file_id = _get_file_id_for_file_path(file_path)
+        #
+        # message = context.bot.send_document(
+        #     chat_id=telegram_user.user_id,
+        #     document=file_id if file_id is not None else open(file_path, 'rb'),
+        #     caption='',
+        #     parse_mode='HTML'
+        # )
+        # _lookup_file_id_in_message(message, file_path, file_id)
 
     # keyboard with 'like' and 'dislike' buttons
     if request_feedback:
@@ -946,7 +1001,6 @@ def send_news_to_telegram_user(context, news_item : NewsItem, telegram_user: Tel
         )
 
     orm_log_news_sent_to_user(news_item, telegram_user)
-
 
     orm_inc_telegram_user_number_received_news_items(telegram_user)
 
@@ -1125,11 +1179,13 @@ def debug3_command_handler(update, context):
 
     fp = '/opt/media/dclavorofvg-bot/uploads/2019/10/23/external-content.duckduckgo.com.jpeg'
 
-    f = open(fp, 'rb')
+    file_id = _get_file_id_for_file_path(fp)
 
-    m = context.bot.send_photo(telegram_user_id, f)
+    print("debug3_command_handler: file_id found? " + str(file_id))
 
-    _process_message_lookup_file_id(m, fp)
+    m = context.bot.send_photo(telegram_user_id, file_id if file_id is not None else open(fp, 'rb'))
+
+    _lookup_file_id_in_message(m, fp, file_id)
 
     # if DEBUG_MSG:
     #     print("debug3_command_handler message:")
