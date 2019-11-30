0af7aaa274cacfffcf4e520d0f2870de6c132c1e
Marco Tessarotto <marcotts@gmail.com>
2019-11-30 18:41:48 +0100

    ml

---
+++ src/ml/spacy_test.py
@@ -0,0 +1,13 @@
+# pip install -U spacy
+# pip install -U spacy-lookups-data
+# python -m spacy download it_core_news_sm
+
+# https://stackoverflow.com/questions/56836865/how-to-use-nlp-in-python-to-analyze-questions-from-a-chat-conversation
+
+"""
+1) Analyze every chat message sentence by sentence. This can be solved with intent classification.
+
+2) Use some kind of matching, like the spaCy's rule based matching This can be solved with entity extraction.
+
+
+"""
